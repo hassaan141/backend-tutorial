f458bb8b436f639109eb27197f3f8dbbf8e73cdd
hassaan141 <hassaanfarooqi2000@gmail.com>
2024-04-23 23:17:34 -0400

    vid7

---
+++ vid7/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express();
+const path = require('path')
+const fs = require('fs');
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.set("view engine", "ejs")
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/', (req, res)=>{
+  fs.readdir(`./files`, (err, files)=>{ //we will put res.render when reading happens
+    console.log(files);
+    res.render("pg1", {files: files}) //you are also sending to pg1 the file that we get
+  })
+})
+
+app.post('/create', (req, res)=>{
+ console.log(req.body);
+ fs.writeFile(`./files/${req.body.title.replaceAll(' ', '')}.txt`, 
+    req.body.body, 
+    (err)=>{
+      res.redirect('/')
+    })
+})
+
+app.listen(3000, ()=>{
+  console.log('Server is running away');
+})
