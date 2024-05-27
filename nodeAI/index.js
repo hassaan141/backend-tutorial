223670faefa59f5e85a0339fb2d484cb640a808d
hassaan141 <hassaanfarooqi2000@gmail.com>
2024-05-26 20:43:03 -0400

    adding API key

--- nodeAI/index.js
+++ nodeAI/index.js
@@ -1,3 +1,13 @@
 const express = require("express")
 const app = express()
 
+app.use(express.json());
+
+app.get("/", (req, res)=>{
+  res.send("Test message")
+})
+
+//1bzrkiPRoGvWmi6yggmIIiO3TT6vbqNVqbVoa43a
+app.listen(3002,()=>{
+  console.log("The backend is running")
+})
