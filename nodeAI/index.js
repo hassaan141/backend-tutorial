862f20f01fa9971265c194634927f6256e326b50
hassaan141 <hassaanfarooqi2000@gmail.com>
2024-05-27 00:11:39 -0400

    working llm

--- nodeAI/index.js
+++ nodeAI/index.js
@@ -1,13 +1,53 @@
-const express = require("express")
-const app = express()
+// const express = require("express")
+// const app = express()
+// const cohere = require("cohere-ai")
+
+// app.use(express.json());
+// cohere.init('1bzrkiPRoGvWmi6yggmIIiO3TT6vbqNVqbVoa43a');
+
+// app.get("/", (req, res)=>{
+//   res.send("Test message")
+// })
+
+// app.get("/test", async (req,res)=>{
+//   const generateRes = await cohere.generate({
+//     model: "large",
+//     prompt: "Who invented the first plane?",
+//     max_tokens: 50,
+//     temperature:1
+//   })
+
+//   res.send(generateRes)
+// })
+
+// app.listen(3002,()=>{
+//   console.log("The backend is running")
+// })
+
+const express = require("express");
+const app = express();
+const { CohereClient } = require('cohere-ai');
+
 
 app.use(express.json());
 
-app.get("/", (req, res)=>{
-  res.send("Test message")
-})
+const cohere = new CohereClient({
+  token: "1bzrkiPRoGvWmi6yggmIIiO3TT6vbqNVqbVoa43a"
+});
 
-//1bzrkiPRoGvWmi6yggmIIiO3TT6vbqNVqbVoa43a
-app.listen(3002,()=>{
-  console.log("The backend is running")
-})
+app.get("/", (req, res) => {
+  res.send("Test a message");
+});
+
+app.get("/test", async (req, res) => {
+  const generate = await cohere.generate({
+    prompt: 'Who invented the bike?',
+  });
+
+  res.send(generate.generations[0].text);
+ 
+});
+
+app.listen(3002, () => {
+  console.log("The backend is running");
+});
