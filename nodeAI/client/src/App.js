2eafabf797174422cd7b9dfbb20638bc937ec6a0
hassaan141 <hassaanfarooqi2000@gmail.com>
2024-05-29 17:50:58 -0400

    Fetch form

--- nodeAI/client/src/App.js
+++ nodeAI/client/src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Enter your question</h1>
+      <div>
+        <form action="https://localhost:3002/test" method="post">
+          <input type="text" name="" id="" />
+        </form>
+      </div>
       <button className="btn btn-primary">Primary Button</button>
     </div>
   );
