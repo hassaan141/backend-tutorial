56e6811b37b8fde1af8dcd9687b8fcef6e27a0df
hassaan141 <hassaanfarooqi2000@gmail.com>
2024-08-20 16:00:14 -0400

    new changes

--- arm.py
+++ arm.py
@@ -1,3 +1,15 @@
 import cv2 as cv
 import numpy as np
 
+cap = cv.VideoCapture(0)
+
+while True:
+    isTrue, frame = cap.read()
+    if isTrue:
+        flipped_frame = cv.flip(frame, 1)  # Flip horizontally
+        cv.imshow("Cam", flipped_frame)
+        if cv.waitKey(1) == ord('d'):
+            break
+
+cap.release()  # Release the camera
+cv.destroyAllWindows()
