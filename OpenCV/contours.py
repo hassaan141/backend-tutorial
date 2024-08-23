145f7a0986b247d4d4b3a1137329907ce07760bc
hassaan141 <hassaanfarooqi2000@gmail.com>
2024-08-22 21:29:15 -0400

    new changes

--- OpenCV/contours.py
+++ OpenCV/contours.py
@@ -1,10 +1,17 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread("Videos/cats.jpg")
+img = cv.imread("Videos/cat.jpg")
 cv.imshow('Boston', img)
 
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Boston2', gray)
 
+canny = cv.Canny(img, 125, 175)
+cv.imshow("Cat", canny)
+
+#countours are the same as edges
+# len(countors) gives you the amount of countours
 
 cv.waitKey(0)
  
